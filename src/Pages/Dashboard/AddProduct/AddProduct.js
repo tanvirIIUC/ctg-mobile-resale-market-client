b9f7144ad29e555dcfa3ac5877a91a75b601526e
tanvirIIUC <tanvirsohaniiuc@gmail.com>
2022-11-26 20:01:42 +0600

    delete and add

--- src/Pages/Dashboard/AddProduct/AddProduct.js
+++ src/Pages/Dashboard/AddProduct/AddProduct.js
@@ -1,10 +1,108 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../../Context/AuthProvider/AuthProvider';
 
 const AddProduct = () => {
+    const { user } = useContext(AuthContext)
+
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        const form = event.target;
+          
+        const seller = form.name.value;
+        const email = form.email.value;
+        const phone = form.phone.value;
+        const title = form.title.value;
+        const place = form.place.value;
+        const resalePrice = form.resaleprice.value;
+        const orginalPrice = form.orginalprice.value;
+        const description = form.description.value;
+        const yearOfUse = form.year.value;
+        const postTime = form.posttime.value;
+        const img = form.img.value;
+         const category = form.category.value;
+
+         const condition = form.condition.value;
+        
+        // console.log(category)
+        //  console.log(seller,email,phone,title,prresalepriceice,orginalprice,description,year,posttime,img,category,condition)
+        const booking = {
+            category_id: category,
+            title,
+            image:img,
+            location:place,
+            resalePrice,
+            orginalPrice,
+            postTime,
+            sellerName:seller,
+            yearOfUse,
+            condition,
+            email,
+            phone,
+            description
+
+        }
+        fetch('http://localhost:5000/addproduct', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(booking)
+
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.acknowledged) {
+
+
+
+                    alert('Add successfull')
+
+
+                    // refetch();
+                }
+                else {
+                    alert(`${data.message}`)
+
+                }
+
+            })
+
+    }
     return (
-        <div>
-            <h1>Add</h1>
-            
+        <div className='w-1/2 container mx-auto'>
+            <h1 className='text-center text-3xl font-bold'>Add</h1>
+            <form onSubmit={handleSubmit} className='grid grid-cols-1 gap-2 my-10  '>
+                <input name='name' type="text" defaultValue={user?.displayName} disabled className="input input-bordered  " />
+                <input name='email' type="email" defaultValue={user?.email} disabled className="input input-bordered w-full " />
+
+                <input name='title' type="text" placeholder="title" required className="input input-bordered w-full " />
+                <input name='resaleprice' type="number" placeholder="resaleprice" required className="input input-bordered w-full " />
+                <input name='orginalprice' type="number" placeholder="orginalprice" required className="input input-bordered w-full " />
+                <input name='description' type="text" placeholder="description" required className="input input-bordered w-full " />
+                <input name='phone' type="text" placeholder="phone" required className="input input-bordered w-full " />
+                <input name='place' type="text" placeholder="place" required className="input input-bordered w-full " />
+                <input name='year' type="text" placeholder="Year of use" required className="input input-bordered w-full " />
+                <input name='posttime' type="text" placeholder="Post time" required className="input input-bordered w-full " />
+               
+                <input name='img' type="text" placeholder="image link" required className="input input-bordered w-full " />
+                <select name='category' className="select select-bordered w-full  ">
+                    <option selected >Category</option>
+                    <option value="01">Samsung</option>
+                    <option value="02">Iphone</option>
+                    <option value="03">Xiaomi</option>
+
+                </select>
+                <select name='condition' className="select select-bordered w-full  ">
+                    <option selected >Condition</option>
+                    <option value="excellent">excellent</option>
+                    <option value="good">Good</option>
+                    <option value="fair">Fair</option>
+
+                </select>
+                <input type="submit" value='submit' className="btn btn-primary w-full text-white  " />
+            </form>
         </div>
     );
 };
--- src/Pages/Dashboard/MyOrders/MyOrders.js
+++ src/Pages/Dashboard/MyOrders/MyOrders.js
@@ -19,48 +19,54 @@ const MyOrders = () => {
     })
     return (
         <div>
-            <h3 className='text-3xl font-bold my-5'>MY Orders</h3>
-            <div className="overflow-x-auto">
-                <table className="table w-full">
-                    {/* <!-- head --> */}
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>Image</th>
-                            <th>Title</th>
-                            <th>Price</th>
-                            <th>Pay</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {/* <!-- row 1 --> */}
-                        <tr>
-                            
-                        </tr>
-                        {
-                            bookings.map((book, i) =>
-                                <tr key={book._id}>
-                                    <th>{i + 1}</th>
-                                    {/* <td>{book.patient}</td> */}
-                                    <td>
-                                        <div className="avatar">
-                                            <div className="mask mask-squircle w-12 h-12">
-                                                <img src={book.img} alt="Avatar Tailwind CSS Component" />
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td>{book.mobileName}</td>
-                                    <td>{book.price}</td>
-                                    <td><button className='btn'>pay</button></td>
+            {
+                user.option === 'user' &&
+                <>
+                    <h3 className='text-3xl font-bold my-5'>MY Orders</h3>
+
+                    <div className="overflow-x-auto">
+                        <table className="table w-full">
+                            {/* <!-- head --> */}
+                            <thead>
+                                <tr>
+                                    <th></th>
+                                    <th>Image</th>
+                                    <th>Title</th>
+                                    <th>Price</th>
+                                    <th>Pay</th>
                                 </tr>
-                            )
-                        }
+                            </thead>
+                            <tbody>
+                                {/* <!-- row 1 --> */}
+                                <tr>
+
+                                </tr>
+                                {
+                                    bookings.map((book, i) =>
+                                        <tr key={book._id}>
+                                            <th>{i + 1}</th>
+                                            {/* <td>{book.patient}</td> */}
+                                            <td>
+                                                <div className="avatar">
+                                                    <div className="mask mask-squircle w-12 h-12">
+                                                        <img src={book.img} alt="Avatar Tailwind CSS Component" />
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td>{book.mobileName}</td>
+                                            <td>{book.price}</td>
+                                            <td><button className='btn'>pay</button></td>
+                                        </tr>
+                                    )
+                                }
 
 
-                    </tbody>
-                </table>
-            </div>
+                            </tbody>
+                        </table>
+                    </div>
+                </>
 
+            }
         </div>
     );
 };
